d2a7b1673aed9de8c82ee2f88b4f4dd4d82c1703
Karla Oshikawa <karla_hara@hotmail.com>
2023-10-18 18:22:06 +0900

    feat: Enviando estudo de Next.js e frammer motion

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     API_URL: "https://652bac64d0d1df5273ee97c2.mockapi.io",
-    DOMAIN_ORIGIN: "process.env.API_URL",
+    DOMAIN_ORIGIN: "one-piece-next-grhb3eao1-karlaoshikawa.vercel.app",
   },
 };
 
